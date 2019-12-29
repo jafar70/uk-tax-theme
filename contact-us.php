f3af67d0eaab4e9880a672b667bec83ee6ec886b
jafsalami <jafsalami@hotmail.co.uk>
2019-12-29 02:27:11 -0500

    Added contact form.

---
+++ contact-us.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="assets/img/uk-tax-pro-logo.png" />
+  <title>Contact Us | UK Tax LTD</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/css/swiper.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Hind:400,500,600,700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+  <div class="top-nav">
+    <div class="top-nav__grid">
+      <ul class="top-nav__grid__item top-nav__grid__item--contact_list">
+        <li><i class="icon-phone"></i> 02085990003</li>
+        <li><i class="icon-paper-plane"></i>vika.accts@gmail.com</li>
+        <li><i class='icon-clock'></i> Mon - Fri: 9:30 - 19:30</li>
+      </ul>
+      <ul class="top-nav__grid__item top-nav__grid__item--social_list">
+        <li><i class='icon-twitter'></i></li>
+        <li><i class="icon-instagram"></i></li>
+      </ul>
+    </div>
+  </div>
+
+  <header id="sticky">
+    <div class="head-container">
+      <img src="assets/img/uk-tax-pro-logo.png" class="logo lazyload" alt="UK Tax Pro Logo">
+      <div id="head-mobile"></div>
+      <div id='button' class='menu button'></div>
+        <ul id='nav' class="nav__links">
+          <li><a href="/">Home</a></li>
+          <li><a href="about-us.php">About Us </a></li>
+          <li><a href="business-start-up.php">Business Start Up </a></li>
+          <li><a href="tax-planning.php">Tax Planning </a></li>
+          <li><a href="accountancy.php">Accountancy </a></li>
+          <li><a href="contact-us.php">Contact Us </a></li>
+        </ul>
+      <a class="cta btn" href="#">Request a Call Back</a>
+    </div>
+  </header>
+
+  <div class="page-banner" style='background: linear-gradient(0deg, rgba(255, 255, 255,0.8), rgba(255, 255, 255,0.8)),url(assets/img/page-banner.jpeg)'>
+    <h1 class="page-banner__title">Contact Us</h1>
+  </div>
+  <div class="breadcrumb">
+    <div class="container">
+      <a href="/">Home</a> > <a href="#!">Contact Us</a> 
+    </div>
+  </div>
+  
+  <section class="full-width-text">
+    <div class="container">
+      <h2 class='underline'>Contact Details</h2>
+      <p>We recently helped a small business grow from break-even to over $1m profit in less than 2 years. Have a general question concerning fortune, but don't know who to contact? Please find below contact details and contact us today!</p>
+    </div>
+    <div class="contact-details">
+    <div class="contact-details__grid container">
+      <div class="contact-details__grid__item">
+        <div class="contact-details-col">
+          <h3>Main Office</h3>
+          <ul class="contact-details-col__list footer__grid__item__contact_list">
+             <li><span class="icon icon-clock"></span><b>Address:</b> 119 Rose Lane, Romford, RM6 5NR</li>
+             <li><span class="icon icon-phone"></span><b>Call Us:</b> 02085990003(Telephone)<br> 07979203598(Mobile)</li>
+             <li><span class="icon icon-paper-plane"></span><b>Mail Us:</b> vika.accts@gmail.com</li>
+            <li><span class="icon icon-clock"></span><b>Opening Time:</b> Mon - Fri: 9:30am - 19:30pm</li>
+           </ul>
+        </div>
+      </div>
+      <div class="contact-details__grid__item">
+        <div id='ez-map'></div>
+      </div>
+    </div>
+  </div>
+    
+   <div class="contact-details-2">
+     <div class="contact-details-2__grid container">
+       <div class="contact-details-2__grid__item">
+         <h2 class='underline'>Feel Free to Contact Us via the form bellow</h2>
+        <form action="" class="form row">
+          <div class="form__col-50">
+            <input type="text" name="name" value="" size="40" class="input-text" id="first-name" placeholder="Name">
+          </div>
+          <div class="form__col-50">
+            <input type="email" name="email" value="" size="40" class="input-text" id="email" placeholder="E-mail Address">
+          </div>
+          <div class="form__col-50">
+            <input type="tel" name="telephone" value="" size="40" class="input-text" id="telephone" placeholder="Telephone Number">
+          </div>
+          <div class="form__col-50">
+            <input type="text" name="subject" value="" size="40" class="input-text" id="subject" placeholder="Subject">
+          </div>
+          <div class="form__col-100">
+            <textarea name="your-message" cols="40" rows="10" class="input-text" id="message" placeholder="Message"></textarea>
+          </div>
+          <div class="form__col-100">
+           <input type="submit" value="Send" class="btn">
+         </div>
+        </form>
+       </div>
+     </div>
+   </div> 
+  </section>
+  
+  <div class='footer' style='background: linear-gradient(0deg, rgba(255, 255, 255,0.85), rgba(255, 255, 255,0.85)),url(assets/img/footer-bg.jpg)'>
+    <div class="footer__grid container">
+      <div class="footer__grid__item">
+        <img data-src="assets/img/uk-tax-pro-logo.png" class="lazyload" alt="UK Tax Pro Logo" loading="lazy">
+        <p>Our team offers the most up-to-date, sustainable manufacturing solutions. teachings of the great explorer of the truth We only source materials from tried and trusted suppliers.</p>
+        <a href="about-us.php" class="default-link">About Us</a>
+      </div>
+      <div class="footer__grid__item">
+        <h3 class="footer__grid__item__title underline underline--left">Services</h3>
+        <ul class="footer__grid__item__list">
+          <li><a href="business-start-up.php">Business Start Up </a></li>
+          <li><a href="tax-planning.php">Tax Planning </a></li>
+          <li><a href="accountancy.php">Accountancy </a></li>
+        </ul>
+      </div>
+      <div class="footer__grid__item">
+        <h3 class="footer__grid__item__title underline underline--left">Legal</h3>
+        <ul class="footer__grid__item__list">
+          <li><a href="#!">Terms & Conditions</a></li>
+          <li><a href="#!">Privacy Policy</a></li>
+        </ul>
+      </div>
+      <div class="footer__grid__item">
+         <h3 class="footer__grid__item__title underline underline--left">Contact Us</h3>
+         <ul class="footer__grid__item__contact_list">
+           <li><span class="icon icon-clock"></span>119 Rose Lane, Romford, RM6 5NR</li>
+           <li><span class="icon icon-phone"></span>02085990003</li>
+           <li><span class="icon icon-paper-plane"></span>vika.accts@gmail.com</li>
+         </ul>
+        <img data-src="assets/img/aat-logo.svg" class="lazyload accreditation-logo" loading="lazy" alt="AAT logo" width='60px'>
+      </div>
+    </div>
+    
+  </div>
+  <div class="footer-credits">
+    <div class="footer-credits__grid container">
+      <div class="footer-credits__grid__item">
+        <p>© 2020 UK Tax Pro LTD All Rights Reserved.</p>
+      </div>
+      <div class="footer-credits__grid__item">
+        <p>
+          Created By <a href="https://jafarsalami.co.uk" target="_blank">Jafar Salami</a>
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/js/swiper.min.js"></script>
+  <script src="assets/js/testimonials-slider.js"></script>
+  <script src="assets/js/nav.js"></script>
+  <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyDbT5LW7F19Ynrs9gGvWk3PoAzsW-xCJcQ'></script>
+  <script src="assets/js/google-maps.js"></script>
+  <script src='assets/js/lazyload.js'></script>
+</body>
+
+</html>
